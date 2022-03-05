096acc76c4e742a25815d69810eb892f7a220de7
maahhi <maahhi.k@gmail.com>
2022-03-04 22:44:56 -0800

    add gitignore

--- vaajoor.py
+++ vaajoor.py
@@ -3,7 +3,15 @@ import numpy as np
 
 
 df = pd.read_csv('a5.csv')
-rand = np.random.randint(len(df))
-answer = df.iloc[rand]
 letters = {'آ', 'ئ', 'ا', 'ب', 'ت', 'ث', 'ج', 'ح', 'خ', 'د', 'ذ', 'ر', 'ز', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع', 'غ', 'ف', 'ق', 'ل', 'م', 'ن', 'ه', 'و', 'پ', 'چ', 'ژ', 'ک', 'گ', 'ی'}
 replacements = {'آ':'ا'}
+
+rand = np.random.randint(len(df))
+answer = df.iloc[rand]
+ans_list = list(answer)
+for i in range(len(ans_list)):
+    for k in replacements.keys():
+        if ans_list[i] == k:
+            ans_list[i] = replacements[k]
+
+guess = input()
